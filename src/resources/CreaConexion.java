bafb3ec9af5806b81800bae8b33f8c9802e5db5a
Axel García <axldorian3@gmail.com>
2022-06-06 19:08:59 -0500

    Para base de datos

---
+++ src/resources/CreaConexion.java
@@ -0,0 +1,29 @@
+package resources;
+
+import java.sql.DriverManager;
+import java.sql.Connection;
+import java.sql.Statement;
+
+public class CreaConexion {
+
+	private Connection c;
+	private Statement stmt;
+
+	public CreaConexion() {
+		String conexion = "jdbc:sqlite::resource:";
+		conexion += getClass().getResource("/resources/").toExternalForm() + "database.db";
+		
+		try {
+			c = DriverManager.getConnection(conexion);
+			stmt = c.createStatement();
+		} catch(Exception e) {
+			System.err.println(e.getClass().getName() + ": " + e.getMessage());
+			System.exit(0);
+		}
+	}
+
+	public Statement getStatement() {
+		return stmt;
+	}
+
+}
