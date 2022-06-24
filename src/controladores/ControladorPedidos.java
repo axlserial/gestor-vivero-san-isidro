21613b183193cb721c080af14f97c7c46a85fd0e
Emanuel <emanuel.palestino.h@gmail.com>
2022-06-24 12:56:19 -0500

    [ControladorPedidos.java] pedidos obtenidos de la base de datos

--- src/controladores/ControladorPedidos.java
+++ src/controladores/ControladorPedidos.java
@@ -1,10 +1,16 @@
 package controladores;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import Entidades.AbonoPago;
 import Entidades.Cliente;
 import Entidades.Pedido;
 import Entidades.Planta;
+import conexiones.ConexionAbono;
+import conexiones.ConexionClientes;
+import conexiones.ConexionPedido;
+import conexiones.ConexionPlantas;
 import interfaces.BusquedaAdministrarPedidos;
 import interfaces.FormularioModificarPedido;
 import javafx.geometry.Pos;
@@ -26,81 +32,14 @@ public class ControladorPedidos {
 	private ArrayList<Pedido> pedidos;
 	private boolean contenido;
 	
+	private final ConexionPedido conexionPedido = new ConexionPedido();
+	private final ConexionClientes conexionClientes = new ConexionClientes();
+	private final ConexionAbono conexionAbono = new ConexionAbono();
+	private final ConexionPlantas conexionPlantas = new ConexionPlantas();
+	
 	public ControladorPedidos(Stage escenario, Scene anterior) {
 		interfazBusqueda = new BusquedaAdministrarPedidos();
-		pedidos = new ArrayList<>();
-
-
-		Pedido ped = new Pedido();
-		Planta[] plants = new Planta[2];
-		AbonoPago[] abpago = new AbonoPago[2];
-		ped.setIdPedido(1);
-		ped.setFechaPedido("2022-06-22");
-		ped.setCliente(new Cliente());
-		plants[0] = new Planta();
-		plants[1] = new Planta();
-		abpago[0] = new AbonoPago();
-		abpago[1] = new AbonoPago();
-		ped.setPlantas(plants);
-		ped.setPagos(abpago);
-		pedidos.add(ped);
-
-		ped = new Pedido();
-		plants = new Planta[2];
-		abpago = new AbonoPago[2];
-		ped.setIdPedido(2);
-		ped.setFechaPedido("2022-06-22");
-		ped.setCliente(new Cliente());
-		plants[0] = new Planta();
-		plants[1] = new Planta();
-		abpago[0] = new AbonoPago();
-		abpago[1] = new AbonoPago();
-		ped.setPlantas(plants);
-		ped.setPagos(abpago);
-		pedidos.add(ped);
-
-		ped = new Pedido();
-		plants = new Planta[2];
-		abpago = new AbonoPago[2];
-		ped.setIdPedido(3);
-		ped.setFechaPedido("2022-06-22");
-		ped.setCliente(new Cliente());
-		plants[0] = new Planta();
-		plants[1] = new Planta();
-		abpago[0] = new AbonoPago();
-		abpago[1] = new AbonoPago();
-		ped.setPlantas(plants);
-		ped.setPagos(abpago);
-		pedidos.add(ped);
-
-		ped = new Pedido();
-		plants = new Planta[2];
-		abpago = new AbonoPago[2];
-		ped.setIdPedido(4);
-		ped.setFechaPedido("2022-06-22");
-		ped.setCliente(new Cliente());
-		plants[0] = new Planta();
-		plants[1] = new Planta();
-		abpago[0] = new AbonoPago();
-		abpago[1] = new AbonoPago();
-		ped.setPlantas(plants);
-		ped.setPagos(abpago);
-		pedidos.add(ped);
-
-		ped = new Pedido();
-		plants = new Planta[2];
-		abpago = new AbonoPago[2];
-		ped.setIdPedido(5);
-		ped.setFechaPedido("2022-06-22");
-		ped.setCliente(new Cliente());
-		plants[0] = new Planta();
-		plants[1] = new Planta();
-		abpago[0] = new AbonoPago();
-		abpago[1] = new AbonoPago();
-		ped.setPlantas(plants);
-		ped.setPagos(abpago);
-		pedidos.add(ped);
-
+		pedidos = new ArrayList<Pedido>();
 
 		interfazBusqueda.principal.setOnAction(e -> {
 			escenario.setScene(anterior);
@@ -115,6 +54,7 @@ public class ControladorPedidos {
 			String nombre = interfazBusqueda.busqueda.getText();
 			String fechaIni = interfazBusqueda.fechaInicial.getValue().toString();
 			String fechaFin = interfazBusqueda.fechaFinal.getValue().toString();
+			pedidos = buscarPedidos(nombre, fechaIni, fechaFin);
 
 			// Establece cantidad de páginas
 			int paginas = pedidos.size() / 4;
@@ -365,4 +305,67 @@ public class ControladorPedidos {
 	public Scene getScene() {
 		return interfazBusqueda.getScene();
 	}
+
+	private ArrayList<Pedido> buscarPedidos(String nombres, String fechaInicial, String fechaFinal) {
+		ResultSet resultado = conexionPedido.buscarPedido(nombres, fechaInicial, fechaFinal);
+		ArrayList<Pedido> res = null;
+		if (resultado != null) {
+			res = new ArrayList<Pedido>();
+			try {
+				// Recorrer Pedidos
+				while (resultado.next()) {
+					Pedido aux = new Pedido();
+					aux.setIdPedido(resultado.getInt("idPedido"));
+					aux.setFechaPedido(resultado.getString("fechaPedido"));
+					Cliente clienteAux = new Cliente();
+					ArrayList<AbonoPago> abonos = new ArrayList<AbonoPago>();
+					ArrayList<Planta> plantas = new ArrayList<Planta>();
+
+					// ObtenerCliente
+					ResultSet clienteRes = conexionClientes.obtenerCliente(resultado.getInt("idCliente"));
+					clienteAux.setIdCliente(clienteRes.getInt("idCliente"));
+					clienteAux.setNombre(clienteRes.getString("nombres"));
+					clienteAux.setApellidos(clienteRes.getString("apellidos"));
+					clienteAux.setPoblacion(clienteRes.getString("poblacion"));
+					String telefonos[] = { clienteRes.getString("telefono1"), clienteRes.getString("telefono2") };
+					clienteAux.setTelefonos(telefonos);
+					aux.setCliente(clienteAux);
+					conexionClientes.cerrarConexion();
+
+					// ObtenerPagos
+					ResultSet abonosRes = conexionAbono.obtenerAbonosDePedido(resultado.getInt("idPedido"));
+					while (abonosRes.next()) {
+						AbonoPago abonoAux = new AbonoPago();
+						abonoAux.setIdAbonosPago(abonosRes.getInt("idAbonosPago"));
+						abonoAux.setFecha(abonosRes.getString("fecha"));
+						abonoAux.setCantidad(abonosRes.getDouble("cantidad"));
+						abonos.add(abonoAux);
+					}
+					aux.setPagos(abonos.toArray(new AbonoPago[0]));
+					conexionAbono.cerrarConexion();
+
+					// Obtener Plantas
+					ResultSet plantasRes = conexionPlantas.obtenerPlantasDePedido(resultado.getInt("idPedido"));
+					while (plantasRes.next()) {
+						Planta plantaAux = new Planta();
+						plantaAux.setIdPlanta(plantasRes.getInt("idPlantaPedido"));
+						plantaAux.setTipoHortaliza(plantasRes.getInt("idTipoHortaliza"));
+						plantaAux.setVariedad(plantasRes.getString("variedad"));
+						plantaAux.setNumeroCharolas(plantasRes.getInt("numeroCharolas"));
+						plantaAux.setPrecio(plantasRes.getDouble("precio"));
+						plantas.add(plantaAux);
+					}
+					aux.setPlantas(plantas.toArray(new Planta[0]));
+					res.add(aux);
+					conexionPlantas.cerrarConexion();
+				}
+				conexionPedido.cerrarConexion();
+			} catch (SQLException e) {
+				e.printStackTrace();
+			}
+		}
+
+		return res;
+	}
+
 }
